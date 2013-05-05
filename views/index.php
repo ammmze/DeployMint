33bc24de93ca959f601095f4b0c1477c204a301e
Luke Stevenson <luke@lucanos.com>
2013-05-05 20:21:14 +1000

    Formatted Views
    Also removed "test.php" file

--- views/index.php
+++ views/index.php
@@ -1,88 +1,91 @@
 <?php
 /*
-    Author: Mark Maunder <mmaunder@gmail.com>
-    Author website: http://markmaunder.com/
-    License: GPL 3.0
+  Author:Mark Maunder <mmaunder@gmail.com>
+  Author website:http://markmaunder.com/
+  License:GPL 3.0
 */
-include dirname(__FILE__) . '/widgets.php';
-?>
-<div id="sdAjaxLoading" style="display: none; position: fixed; right: 1px; top: 1px; width: 100px; background-color: #F00; color: #FFF; font-size: 12px; font-family: Verdana, arial; font-weight: normal; text-align: center; z-index: 100; border: 1px solid #CCC;">Loading...</div>
-<div class="wrap">
-    <h2 class="depmintHead">Manage Projects</h2>
-    <form>
-        <input type="hidden" name="action" value="deploymint_createProject" />
-        <table class="form-table deploymintTable">
-        <tr>
-            <td>Enter the name of a project to create:</td>
-            <td><input type="text" name="name" id="sdProjectName" value="" size="55" maxlength="100" /></td>
-        </tr>
-        <tr>
-            <td>Git Origin Location:</td>
-            <td><input type="text" name="origin" id="sdProjectOrigin" value="" size="55" maxlength="255" /></td>
-        </tr>
-        <tr>
-            <td>Additional Tables:</td>
-            <td><input type="text" name="tables" value="" size="55" maxlength="255" /></td>
-        </tr>
-        <tr class="note">
-            <td colspan=2>
-                Additional tables to include the snapshots (comma separated and exclude the table prefix, if the table uses a different prefix or no prefix, then use a backslash before the table name. ie \custom_table). Default tables included in the snapshot are: <?php echo implode(', ', $this->getTableList()); ?>
-            </td>
-        </tr>
-        <tr>
-            <td colspan=2><input type="button" name="but2" value="Create project" onclick="deploymint.createProject(jQuery(this).parents('form:first').serializeObject()); return false;" class="button-primary" /></td>
-        </tr>
-        </table>
-    </form>
-    <p id="sdProjects">
-    </p>
 
-        
+include( dirname( __FILE__ ) . '/widgets.php' );
+
+?>
+<div id="sdAjaxLoading" style="display:none;position:fixed;right:1px;top:1px;width:100px;background-color:#F00;color:#FFF;font-size:12px;font-family:Verdana, arial;font-weight:normal;text-align:center;z-index:100;border:1px solid #CCC;">Loading...</div>
+<div class="wrap">
+
+  <h2 class="depmintHead">Manage Projects</h2>
+  <form>
+    <input type="hidden" name="action" value="deploymint_createProject" />
+    <table class="form-table deploymintTable">
+      <tr>
+        <td>Enter the name of a project to create:</td>
+        <td><input type="text" name="name" id="sdProjectName" value="" size="55" maxlength="100" /></td>
+      </tr>
+      <tr>
+        <td>Git Origin Location:</td>
+        <td><input type="text" name="origin" id="sdProjectOrigin" value="" size="55" maxlength="255" /></td>
+      </tr>
+      <tr>
+        <td>Additional Tables:</td>
+        <td><input type="text" name="tables" value="" size="55" maxlength="255" /></td>
+      </tr>
+      <tr class="note">
+        <td colspan="2">Additional tables to include the snapshots (comma separated and exclude
+          the table prefix, if the table uses a different prefix or no prefix, then use a backslash
+          before the table name. ie \custom_table). Default tables included in the snapshot are:
+          <?php echo implode(', ', $this->getTableList());?></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input type="button" name="but2" value="Create project" onclick="deploymint.createProject(jQuery(this).parents('form:first').serializeObject());return false;" class="button-primary" /></td>
+      </tr>
+    </table>
+  </form>
+
+  <p id="sdProjects"></p>
+
 </div>
 <script type="text/x-jquery-tmpl" id="sdProjTmpl">
 <div id="sdProj${id}">
 {{each(i,proj) projects}}
-<h2>Project: ${proj.name}&nbsp;<a href="#" onclick="deploymint.deleteProject(${proj.id}); return false;" style="font-size: 10px;">remove</a></h2>
+<h2>Project:${proj.name}&nbsp;<a href="#" onclick="deploymint.deleteProject(${proj.id});return false;" style="font-size:10px;">remove</a></h2>
 <div class="depProjWrap">
-    {{if proj.project_uuid}}
-    <div>UUID: ${proj.project_uuid}</div>
+  {{if proj.project_uuid}}
+  <div>UUID:${proj.project_uuid}</div>
+  {{/if}}
+  {{if proj.origin}}
+  <div>
+    Origin:
+    <span class="deploymint-origin" title="${proj.originAvailableMessage}">
+    {{if proj.originAvailable}}
+      <span class="deploymint-success">${proj.origin}</span>
+    {{else}}
+      <span class="deploymint-error">${proj.origin} -- Unable to connect</span>
     {{/if}}
-    {{if proj.origin}}
-    <div>
-        Origin: 
-        <span class="deploymint-origin" title="${proj.originAvailableMessage}">
-        {{if proj.originAvailable}}
-            <span class="deploymint-success">${proj.origin}</span>
-        {{else}}
-            <span class="deploymint-error">${proj.origin} -- Unable to connect</span>
-        {{/if}}
-        </span>
-        <input type="button" name="edit-origin" value="Edit Origin" class="button-secondary" onclick="deploymint.editOrigin(${proj.id}, '${proj.origin}')" />
-    </div>
-    {{/if}}
-    <div>
-        Tables: ${proj.tables}<input type="button" name="edit-tables" value="Edit Tables" class="button-secondary" onclick="deploymint.editTables(${proj.id}, '${proj.tables}')" />
-    </div>
-    <br />
-    Add a blog to this project:&nbsp;<select id="projAddSel${proj.id}">
-    {{if proj.numNonmembers}}
-    {{each(k,blog) proj.nonmemberBlogs}}
-    <option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
+    </span>
+    <input type="button" name="edit-origin" value="Edit Origin" class="button-secondary" onclick="deploymint.editOrigin(${proj.id}, '${proj.origin}')" />
+  </div>
+  {{/if}}
+  <div>
+    Tables:${proj.tables}<input type="button" name="edit-tables" value="Edit Tables" class="button-secondary" onclick="deploymint.editTables(${proj.id}, '${proj.tables}')" />
+  </div>
+  <br />
+  Add a blog to this project:&nbsp;<select id="projAddSel${proj.id}">
+  {{if proj.numNonmembers}}
+  {{each(k,blog) proj.nonmemberBlogs}}
+  <option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
+  {{/each}}
+  {{else}}
+  <option value="">--No blogs left to add--</option>
+  {{/if}}
+  </select>&nbsp;<input type="button" name="but12" value="Add this blog to the project" onclick="deploymint.addBlogToProject({projectID:${proj.id}, blogID:jQuery('#projAddSel${proj.id}').val()});return false;" />
+  <h3 class="depSmallHead">Blogs that are part of this project:</h3>
+  {{if proj.memberBlogs.length}}
+  <ul class="depList">
+    {{each(l,blog) proj.memberBlogs}}
+    <li>${blog.domain}${blog.path}&nbsp;<a href="#" onclick="deploymint.removeBlogFromProject({projectID:${proj.id}, blogID:${blog.blog_id}});return false;" style="font-size:10px;">remove</a></li>
     {{/each}}
-    {{else}}
-    <option value="">--No blogs left to add--</option>
-    {{/if}}
-    </select>&nbsp;<input type="button" name="but12" value="Add this blog to the project" onclick="deploymint.addBlogToProject({projectID:${proj.id}, blogID:jQuery('#projAddSel${proj.id}').val()}); return false;" />
-    <h3 class="depSmallHead">Blogs that are part of this project:</h3>
-    {{if proj.memberBlogs.length}}
-    <ul class="depList">
-        {{each(l,blog) proj.memberBlogs}}
-        <li>${blog.domain}${blog.path}&nbsp;<a href="#" onclick="deploymint.removeBlogFromProject({projectID:${proj.id}, blogID:${blog.blog_id}}); return false;" style="font-size: 10px;">remove</a></li>
-        {{/each}}
-    </ul>
-    {{else}}
-    <i>&nbsp;&nbsp;You have not added any blogs to this project yet.</i>
-    {{/if}}
+  </ul>
+  {{else}}
+  <i>&nbsp;&nbsp;You have not added any blogs to this project yet.</i>
+  {{/if}}
 </div>
 {{/each}}
 
@@ -90,6 +93,8 @@ include dirname(__FILE__) . '/widgets.php';
 </script>
 <script type="text/javascript">
 jQuery(function(){
-    deploymint.reloadProjects();
-    });
+
+  deploymint.reloadProjects();
+
+});
 </script>
--- views/manageProject.php
+++ views/manageProject.php
@@ -1,105 +1,114 @@
 <?php
 /*
-    Author: Mark Maunder <mmaunder@gmail.com>
-    Author website: http://markmaunder.com/
-    License: GPL 3.0
+  Author:Mark Maunder <mmaunder@gmail.com>
+  Author website:http://markmaunder.com/
+  License:GPL 3.0
 */
-include dirname(__FILE__) . '/widgets.php';
+include( dirname( __FILE__ ) . '/widgets.php' );
 ?>
 
-<div id="sdAjaxLoading" style="display: none; position: fixed; right: 1px; top: 1px; width: 100px; background-color: #F00; color: #FFF; font-size: 12px; font-family: Verdana, arial; font-weight: normal; text-align: center; z-index: 100; border: 1px solid #CCC;">Loading...</div>
+<div id="sdAjaxLoading" style="display:none;position:fixed;right:1px;top:1px;width:100px;background-color:#F00;color:#FFF;font-size:12px;font-family:Verdana, arial;font-weight:normal;text-align:center;z-index:100;border:1px solid #CCC;">Loading...</div>
 <div class="wrap">
-<h2 class="depmintHead">DepoyMint Project: &#8220;<?php echo $proj['name'] ?>&#8221;</h2> 
 
-<h3>Create a snapshot from a blog:</h3>
-<form id="sdCreateSnapshot"></form>
+  <h2 class="depmintHead">DepoyMint Project:&#8220;<?php echo $proj['name'] ?>&#8221;</h2> 
 
-<h3>Deploy a snapshot to a blog:</h3>
-<form id="sdDeploySnapshot">
-</form>
+  <h3>Create a snapshot from a blog:</h3>
+  <form id="sdCreateSnapshot"></form>
+
+  <h3>Deploy a snapshot to a blog:</h3>
+  <form id="sdDeploySnapshot"></form>
 
 </div>
 <script type="text/x-jquery-tmpl" id="sdDeploySnapTmpl">
 <input type="hidden" name="action" value="deploymint_deploySnapshot" />
 <input type="hidden" name="projectid" value="<?php echo $proj['id'];?>" />
 <table class="form-table deploymintTable">
-<tr>
+  <tr>
     <td>Select a snapshot to deploy:</td>
-    <td><select id="sdDepSnapshot" name="name" onchange="deploymint.updateSnapDesc(<?php echo $proj['id'];?>, jQuery(this).val()); return true;">
-        {{if snapshots.length}}
+    <td>
+      <select id="sdDepSnapshot" name="name" onchange="deploymint.updateSnapDesc(<?php echo $proj['id'];?>, jQuery(this).val());return true;">
+      {{if snapshots.length}}
         {{each(i,snap) snapshots}}
-        <option value="${snap.name}"{{if selectedSnap == snap.name}} selected{{/if}}>${snap.name} - Created on: ${snap.created}</option>
+        <option value="${snap.name}"{{if selectedSnap == snap.name}} selected{{/if}}>${snap.name} - Created on:${snap.created}</option>
         {{/each}}
-        {{else}}
+      {{else}}
         <option value="">--No snapshots created yet--</option>
-        {{/if}}
-        </select> <a href="javascript:void(0)" name='archiveSnapshot'>archive this snapshot</a>
+      {{/if}}
+      </select>
+      <a href="javascript:void(0)" name="archiveSnapshot">archive this snapshot</a>
     </td>
-</tr>
-<tr>
+  </tr>
+  <tr>
     <td>Currently selected snapshot description:</td>
     <td>
-        <textarea rows="5" cols="50" style="width: 550px; height: 200px;" id="sdSnapDesc2" READONLY></textarea>
+      <textarea rows="5" cols="50" style="width:550px;height:200px;" id="sdSnapDesc2" readonly="readonly"></textarea>
     </td>
-</tr>
-<tr>
+  </tr>
+  <tr>
     <td>Select what to deploy:</td>
     <td>
     {{each(i,opt) deployParts}}
-        <label class="deployPart">
-            <input type="checkbox" value="1" checked="checked" name="deployParts[${i}]" />
-            ${opt}
-        </label>
+      <label class="deployPart">
+        <input type="checkbox" value="1" checked="checked" name="deployParts[${i}]" />
+        ${opt}
+      </label>
     {{/each}}
     </td>
-</tr>
-<tr>
+  </tr>
+  <tr>
     <td>Select a blog to deploy to:</td>
-    <td><select id="sdDepBlog" name="blogid">
-{{each(i,blog) blogs}}
-<option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
-{{/each}}
-</select>
+    <td>
+      <select id="sdDepBlog" name="blogid">
+      {{each(i,blog) blogs}}
+        <option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
+      {{/each}}
+      </select>
     </td>
-</tr>
-<tr><td colspan="2">
-    <input type="button" value="Deploy this snapshot to the selected blog" onclick="deploymint.deploySnapshot(jQuery('#sdDeploySnapshot').serializeObject()); return false;" class="button-primary" />
-</td></tr>
+  </tr>
+  <tr>
+    <td colspan="2">
+      <input type="button" value="Deploy this snapshot to the selected blog" onclick="deploymint.deploySnapshot(jQuery('#sdDeploySnapshot').serializeObject());return false;" class="button-primary" />
+    </td>
+  </tr>
 </table>
 </script>
-
 <script type="text/x-jquery-tmpl" id="sdCreateSnapTmpl">
 <table class="form-table deploymintTable">
-<tr>
+  <tr>
     <td>Select a blog to snapshot:</td>
-    <td><select id="sdSnapBlog">
-{{if blogs.length}}
-{{each(i,blog) blogs}}
-<option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
-{{/each}}
-{{else}}
-<option value="">--Please add a blog to this project--</option>
-{{/if}}
-</select>
+    <td>
+      <select id="sdSnapBlog">
+  {{if blogs.length}}
+    {{each(i,blog) blogs}}
+        <option value="${blog.blog_id}">${blog.domain}${blog.path}</option>
+    {{/each}}
+  {{else}}
+        <option value="">--Please add a blog to this project--</option>
+  {{/if}}
+      </select>
     </td>
-</tr>
-<tr>
+  </tr>
+  <tr>
     <td>Enter a short unique name for your snapshot<br />(only a-z, A-Z, 0-9 and .-_ chars allowed. No spaces.):</td>
     <td><input type="text" id="sdSnapName" size="20" maxlength="20" /></td>
-</tr>
-<tr>
+  </tr>
+  <tr>
     <td>Enter a description for your snapshot (text only):</td>
     <td><textarea id="sdSnapDesc" rows="5" cols="60"></textarea></td>
-</tr>
-<tr><td colspan="2">
-    <input type="button" value="Create this snapshot" onclick="deploymint.createSnapshot({projectid:projectid, blogid:jQuery('#sdSnapBlog').val(), name:jQuery('#sdSnapName').val(), desc:jQuery('#sdSnapDesc').val()}); return false;" class="button-primary" />
-</td></tr>
+  </tr>
+  <tr>
+    <td colspan="2">
+      <input type="button" value="Create this snapshot" onclick="deploymint.createSnapshot({projectid:projectid, blogid:jQuery('#sdSnapBlog').val(), name:jQuery('#sdSnapName').val(), desc:jQuery('#sdSnapDesc').val()});return false;" class="button-primary" />
+    </td>
+  </tr>
 </table>
 </script>
 <script type="text/javascript">
 var projectid = <?php echo $proj['id'] ?>;
-jQuery(function(){ 
-    deploymint.updateCreateSnapshot(projectid); 
-    deploymint.updateDeploySnapshot(projectid); 
+jQuery(function(){
+
+  deploymint.updateCreateSnapshot(projectid);
+  deploymint.updateDeploySnapshot(projectid);
+
 });
 </script>
